4295f452978bddd59e38e3501ca367c74d07ff80
raulpy271 <56479285+raulpy271@users.noreply.github.com>
2020-08-03 13:43:05 -0300

    Add files via upload

---
+++ Tools/Decompose.hs
@@ -0,0 +1,64 @@
+module Tools.Decompose 
+  ( decomposeFormulaInString
+  ) 
+  where
+
+
+import Text.Regex.Posix
+import Tools.HelperTools 
+  ( sublist
+  , getIndexOfFirstOperation
+  , getIndexOfLastOperation
+  , getIndexOfOperands
+  )
+
+
+
+decomposeFormulaInString :: String -> [String]
+
+--e. g.:"(p > 1) = (a = b)" -> ["p > 1", " = ", "a = b"  ]
+
+decomposeFormulaInString string = result (getIndexOfOperands string) string
+  where
+    indexFirstOperation = getIndexOfFirstOperation string
+    indexLastOperation = getIndexOfLastOperation string
+    result list string
+      -- caso a formula seja desta forma : (p > q) = (p > q)
+      | length list == 4 = 
+          [ sublist ((list!!0)+1) (list!!1) string
+          , sublist ((list!!1)+1) (list!!2) string
+          , sublist ((list!!2)+1) (list!!3) string
+          ] 
+
+
+      | length list == 2 =
+        -- caso a formula seja assim: ~ ( p > q)
+        if (string =~ "\\w|\\(" :: String) == "(" 
+          && ((reverse string) =~ "\\w|\\)" :: String) == ")"
+          then [ sublist ((list!!0)+1) (list!!1) string, " ", " " ]
+          else
+            if (string =~ "\\w|\\(" :: String) == "("
+            -- caso a formula seja assim : (p = q) > s
+            then 
+              [ sublist ((list!!0)+1) (list!!1) string
+              , sublist indexLastOperation (indexLastOperation+1)  string
+              , sublist (indexLastOperation+1) (length string) string 
+              ]
+          -- caso a formula seja assim: p > (q = s)
+            else 
+              [ sublist 0 indexFirstOperation string
+              , sublist indexFirstOperation (indexFirstOperation+2) string
+              , sublist ((list!!0)+1) (list!!1) string 
+              ]
+
+
+      -- caso a formula seja assim: p > q
+      | (null list) && indexFirstOperation /= (-1) =
+        [ sublist 0 indexFirstOperation string
+        , [string !! indexFirstOperation ]
+        , sublist (indexFirstOperation + 1) (length string) string 
+        ]
+
+
+      | otherwise = [string, " ", " "]  
+
---
+++ Tools/HelperTools.hs
@@ -0,0 +1,73 @@
+module Tools.HelperTools 
+  ( sublist
+  , getIndexOfFirstOperation
+  , getIndexOfLastOperation
+  , getIndexOfOperands 
+  , getComplexity
+  ) 
+  where
+
+
+import Text.Regex.Posix
+
+
+type FormulaInString = String
+
+
+getComplexity :: FormulaInString -> Int
+getComplexity p = p =~ "[V^~>=]" :: Int
+
+
+removeDuplicate string = remove string []
+  where
+    remove (x:xs) newstring
+      | null xs = newstring ++ [x]
+      | elem x xs = remove xs newstring
+      | otherwise = remove xs (newstring ++ [x])
+
+
+getPreposicoes :: FormulaInString -> String
+-- e.g: getPreposicoes "(p > q) = s" >---> "pqs"
+getPreposicoes string = 
+  if null preposicoes 
+    then ""
+    else removeDuplicate (convertlist2string preposicoes "")
+  where
+    preposicoes = string =~ "[a-z]" :: [[String]]
+    convertlist2string :: [[String]] -> String -> String
+    convertlist2string (x:xs) string
+      | null xs = (x !! 0) ++ string
+      | otherwise = convertlist2string xs ((x !! 0) ++ string) 
+
+
+sublist start end text
+  | (end > length text) || start < 0 = "" 
+  | otherwise = take (end - start) (drop start text)
+
+
+getIndexOfLastOperation :: FormulaInString -> Int
+getIndexOfLastOperation s = 
+  (length s - 1) - fst (reverse s =~ "[V^=~>]" :: (Int, Int))
+getIndexOfFirstOperation :: FormulaInString -> Int
+getIndexOfFirstOperation s = fst (s =~ "[V^>=~]" :: (Int, Int))
+
+getIndexOfOperands :: FormulaInString -> [Int]
+getIndexOfOperands string = decompor string 0 0 0 []
+
+-- retorna lista com indices dos operandos
+-- e.g.: "(p > q) = (a > c)" -> [0, 6, 10, 16]
+--        ^     ^   ^     ^
+  where
+    
+    decompor string index openeds paresFechados list
+      | index >= length string = list
+      | string !! index == '(' = 
+        if openeds == 0 
+          then decompor string (index+1) (openeds + 1) paresFechados (list ++ [index])
+        else decompor string (index+1) (openeds + 1) paresFechados list
+      | string !! index  == ')' = 
+        if (openeds - 1) == 0
+          then decompor string (index+1) (openeds - 1) (paresFechados+1) (list ++ [index])
+        else decompor string (index+1) (openeds -1) paresFechados list
+      | otherwise = decompor string (index+1) openeds paresFechados list
+
