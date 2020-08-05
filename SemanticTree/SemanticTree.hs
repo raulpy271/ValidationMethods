bac404bd8f5f9b55404430989de54e92c04d2475
raulpy271 <56479285+raulpy271@users.noreply.github.com>
2020-08-05 14:33:45 -0300

    Update SemanticTree.hs

--- SemanticTree/SemanticTree.hs
+++ SemanticTree/SemanticTree.hs
@@ -1,10 +1,54 @@
-module SemancticTree
+module SemancticTree.SemancticTree
   ( isTautology
   )
   where
 
-import DataWFF.ProposicionalCalculus
+import DataWFF.PropositionalCalculus
+import DataWFF.Interpretation (interpretation)
+import Tools.DataWFFManipulation 
+  ( getUnkownPrepositionInWFF 
+  , replacePrepositionInFormula
+  )
+
+
+(truePrepositionValue, falsePrepositionValue) = 
+  ( PrepositionWithKnownValue '1' True
+  , PrepositionWithKnownValue '0' False
+  )
 
 
 isTautology :: WFF Preposition -> WFFProperties
-isTautology wff = undefined
+isTautology wff 
+  | not (elem truePrepositionValue allInterpretation) = Contradiction
+  | not (elem falsePrepositionValue allInterpretation) = Tautology
+  | otherwise = Satisfiable 
+  where allInterpretation = allInterpretationOfWFF wff
+
+
+allInterpretationOfWFF :: WFF Preposition -> [Preposition]
+allInterpretationOfWFF wff = 
+  allInterpretationHelper wff (getUnkownPrepositionInWFF wff) 
+
+  where
+    allInterpretationHelper 
+      :: WFF Preposition -> [Preposition] -> [Preposition] 
+    allInterpretationHelper wff [] = [interpretation wff]
+
+
+    allInterpretationHelper wff (firstPreposition:otherPreposition)
+      | wffInterpretation == truePrepositionValue = [truePrepositionValue]
+      | wffInterpretation == falsePrepositionValue = [falsePrepositionValue]
+      | otherwise =
+          (allInterpretationHelper wffWithOne otherPreposition) ++ 
+          (allInterpretationHelper wffWithZero otherPreposition)
+
+      where
+        wffInterpretation = interpretation wff
+        (wffWithOne, wffWithZero) = 
+          ( replacePrepositionInFormula wff
+            firstPreposition truePrepositionValue 
+          , replacePrepositionInFormula wff
+            firstPreposition falsePrepositionValue 
+          )
+
+
