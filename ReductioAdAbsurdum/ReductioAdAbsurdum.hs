75581efb1983f39e5e0ad17054e0a8ec46e194a5
raulpy271 <56479285+raulpy271@users.noreply.github.com>
2020-08-15 13:31:45 -0300

    Update ReductioAdAbsurdum.hs

--- ReductioAdAbsurdum/ReductioAdAbsurdum.hs
+++ ReductioAdAbsurdum/ReductioAdAbsurdum.hs
@@ -1,10 +1,43 @@
-module ReductionAdAbsurdum
+module ReductioAdAbsurdum.ReductioAdAbsurdum
   ( isTautology
+  , viewReductioAdAbsurdumProcess
   )
   where
 
-import DataWFF.ProposicionalCalculus
+
+import DataWFF.PropositionalCalculus
+import Data.Maybe (isJust)
+import ReductioAdAbsurdum.ViewReductioAdAbsurdum 
+  ( viewReductioAdAbsurdumProcess
+  )
+import ReductioAdAbsurdum.SearchInfoAboutWFF
+  ( PairOfPrepositionAndYourValue 
+  , getInfoAssumingItsNotTautology 
+  , applyingDiscoveredValuesInWFF 
+  , searchForAbsurdInList
+  )
 
 
-isTautology :: WFF Preposition -> WFFProperties
-isTautology wff = undefined
+isTautology ::  WFF Preposition -> WFFProperties
+isTautology wff
+  | isJust maybeAbsurd = Tautology
+  | otherwise = Unknown
+  where maybeAbsurd = searchForAbsurdInWFF wff
+
+
+searchForAbsurdInWFF :: WFF Preposition -> Maybe Preposition
+searchForAbsurdInWFF wff  
+  | null listOfPairPrepositionValueFound = Nothing
+  | hasAbsurd = absurdFound
+  | otherwise = searchForAbsurdInWFF newWff
+  where
+    newWff = 
+      applyingDiscoveredValuesInWFF wff 
+      listOfPairPrepositionValueFound
+    listOfPairPrepositionValueFound = 
+      getInfoAssumingItsNotTautology wff
+    absurdFound = 
+      searchForAbsurdInList listOfPairPrepositionValueFound
+    hasAbsurd = isJust absurdFound
+
+
