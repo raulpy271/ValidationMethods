229fb6cba60190130b2deb731a6c5168a4b249bc
raulpy271 <56479285+raulpy271@users.noreply.github.com>
2020-08-17 14:48:27 -0300

    Update DataWFFManipulation.hs

--- Tools/DataWFFManipulation.hs
+++ Tools/DataWFFManipulation.hs
@@ -1,5 +1,6 @@
 module Tools.DataWFFManipulation 
   ( mapWFF
+  , foldWFF
   , getAllAtomicPreposition 
   , replacePrepositionInFormula 
   , getUnkownPreposition 
@@ -7,6 +8,8 @@ module Tools.DataWFFManipulation
   , truePrepositionValue
   , falsePrepositionValue
   , negateWFF 
+  , hasUnknownPrepositionInWFF
+  , hasKnownPrepositionInWFF
   )
   where
 
@@ -26,6 +29,23 @@ mapWFF f (And left rigth)     = And (mapWFF f left) (mapWFF f rigth)
 mapWFF f (Or left rigth)      = Or (mapWFF f left) (mapWFF f rigth)
 mapWFF f (Iff left rigth)     = Iff (mapWFF f left) (mapWFF f rigth)
 mapWFF f (Imply left rigth)   = Imply (mapWFF f left) (mapWFF f rigth)
+
+
+foldWFF 
+  :: (Preposition -> Preposition -> Preposition) 
+  -> Preposition 
+  -> WFF Preposition 
+  ->  Preposition 
+foldWFF f acumulator (Atomic preposition) = f acumulator preposition
+foldWFF f acumulator (Not wff) = foldWFF f acumulator wff
+foldWFF f acumulator (And left rigth) = 
+  foldWFF f (foldWFF f acumulator left) rigth
+foldWFF f acumulator (Or left rigth) = 
+  foldWFF f (foldWFF f acumulator left) rigth
+foldWFF f acumulator (Iff left rigth) = 
+  foldWFF f (foldWFF f acumulator left) rigth
+foldWFF f acumulator (Imply left rigth) = 
+  foldWFF f (foldWFF f acumulator left) rigth
 
 
 getAllAtomicPreposition :: WFF Preposition -> [Preposition]
@@ -93,4 +113,53 @@ getUnkownPrepositionInWFF = (getUnkownPreposition . getAllAtomicPreposition)
 negateWFF :: WFF Preposition -> WFF Preposition  
 negateWFF wff = Not (wff)
 
+
+convertPrepositionToBool :: Preposition -> Bool
+convertPrepositionToBool (PrepositionWithKnownValue _ True) = True
+convertPrepositionToBool _ = False
+
+
+hasKnownPreposition :: Preposition -> Bool
+hasKnownPreposition (PrepositionWithKnownValue _ _ ) = True
+hasKnownPreposition _ = False
+hasKnownPreposition2 left rigth 
+  = (hasKnownPreposition left) 
+  || (hasKnownPreposition rigth)
+
+
+hasKnownPrepositionInWFF :: WFF Preposition -> Bool
+hasKnownPrepositionInWFF wff 
+  = convertPrepositionToBool 
+  $ foldWFF checkIfHasPrepositionAndConvertBoolToPreposition identity wff 
+  where
+    identity = PrepositionWithUnknownValue 'i'
+    getKnownPrepositionIfParameterIsTrue True = truePrepositionValue
+    getKnownPrepositionIfParameterIsTrue _ = identity
+    checkIfHasPrepositionAndConvertBoolToPreposition 
+      :: Preposition -> Preposition -> Preposition
+    checkIfHasPrepositionAndConvertBoolToPreposition left rigth 
+      = getKnownPrepositionIfParameterIsTrue
+      $ (hasKnownPreposition2 left rigth)
+
+
+hasUnknownPreposition2 left rigth
+  = (not $ hasKnownPreposition left)
+  || (not $ hasKnownPreposition rigth)
+
+
+hasUnknownPrepositionInWFF :: WFF Preposition -> Bool
+hasUnknownPrepositionInWFF wff
+  = not
+  $ convertPrepositionToBool
+  $ foldWFF checkIfHasPrepositionAndConvertBoolToPreposition identity wff 
+  where
+    identity = truePrepositionValue
+    getUnkownPrepositionIfParameterIsTrue True = 
+      PrepositionWithUnknownValue 's'
+    getUnkownPrepositionIfParameterIsTrue _ = identity
+    checkIfHasPrepositionAndConvertBoolToPreposition 
+      :: Preposition -> Preposition -> Preposition
+    checkIfHasPrepositionAndConvertBoolToPreposition left rigth 
+      = getUnkownPrepositionIfParameterIsTrue
+      $ (hasUnknownPreposition2 left rigth)
 
